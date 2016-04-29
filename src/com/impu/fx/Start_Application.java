43a59c27ad9173f8190805e041b16f5151cc2e8f
Dimibe <dimitribegnis@gmail.com>
2016-04-29 19:08:01 +0200

    fixed conflicts

--- src/com/impu/fx/Start_Application.java
+++ src/com/impu/fx/Start_Application.java
@@ -1,69 +1,3 @@
-<<<<<<< Upstream, based on branch 'master' of https://github.com/Dimibe/ImageTools.git
-package com.impu.fx;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class Start_Application extends Application implements Runnable {
-
-	private Stage primaryStage;
-	GraphicsContext gc;
-	Canvas canvas;
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		this.primaryStage = primaryStage;
-		primaryStage.setTitle("Fivas");
-		
-		StackPane root = new StackPane();
-		canvas = new Canvas();
-		canvas.widthProperty().bind(root.widthProperty());
-		canvas.heightProperty().bind(root.heightProperty());
-		gc = canvas.getGraphicsContext2D();
-		root.getChildren().add(canvas);
-		primaryStage.setScene(new Scene(root));
-		primaryStage.show();
-
-		new Thread(this).start();
-	}
-
-	private void drawShapes(GraphicsContext g) {
-		g.drawImage(Controller.getInstance().getImage(), 0, 0, getWidth(), getHeight());
-	}
-
-	private double getHeight() {
-		return primaryStage.getHeight();
-	}
-
-	private double getWidth() {
-		return primaryStage.getWidth();
-	}
-
-	@Override
-	public void run() {
-		while (true) {
-
-			drawShapes(canvas.getGraphicsContext2D());
-
-			try {
-				Thread.sleep(15);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
-
-}
-=======
 package com.impu.fx;
 
 import javafx.application.Application;
@@ -130,4 +64,3 @@ public class Start_Application extends Application implements Runnable {
 	}
 
 }
->>>>>>> 5e05e44 Cleaning up 
